206afe99656cb346bfb5fb8757d01bb548f8c07f
gpavicic <goran.pavicic@gmail.com>
2017-08-14 21:43:15 +0200

    Postgre to MySQL setup

--- src/main/java/com/company/goxy/model/Adress.java
+++ src/main/java/com/company/goxy/model/Adress.java
@@ -1,20 +1,12 @@
 package com.company.goxy.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import com.company.goxy.model.User;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "`adress`")
